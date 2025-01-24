4e52f0d3c3884b692d7ae6b06849643a0954e34b
bigcherry24 <cherry24mint@naver.com>
2025-01-24 01:01:17 +0000

    AppTest 수정

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,25 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class AppTest {
 
     @Test
-    public void testMainMethod() {
-        // You can add tests for the App class methods here
-        assertEquals(1, 1); // Example test
+    void testMain() {
+        // main 메서드를 테스트하기 위해 System.out을 캡처합니다.
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // main 메서드 실행
+        App.main(new String[]{});
+
+        // 출력된 내용 확인
+        assertEquals("Hello, World!\n", outContent.toString());
+
+        // 원래의 System.out으로 복원
+        System.setOut(originalOut);
     }
 }
